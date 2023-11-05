7ac6608697b79d270753c1d208585dfbb9604937
humberto3389 <114783668+humberto3389@users.noreply.github.com>
2023-11-05 10:06:13 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include 'Vista/includes/header.php';
+include 'Vista/includes/contenido.php';
+include 'Vista/includes/footer.php';
+include 'Vista/includes/modal.php';
+include 'Vista/includes/icono-flotante.php';
+?>
+
